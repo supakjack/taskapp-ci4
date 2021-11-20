2f4850d174bb51e6de4e8770b02ae14aab16d31d
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2021-11-20 22:10:38 +0700

    send data to controller
    send data to controller

--- taskapp/app/Controllers/Tasks.php
+++ taskapp/app/Controllers/Tasks.php
@@ -6,6 +6,11 @@ class Tasks extends BaseController
 {
     public function index()
     {
-        return view("Tasks/index");
+        $data = [
+            ['id' => 1, 'description' => 'first task'],
+            ['id' => 2, 'description' => 'second task']
+        ];
+        
+        return view("Tasks/index", ['tasks' => $data]);
     }
 }
--- taskapp/app/Views/Tasks/index.php
+++ taskapp/app/Views/Tasks/index.php
@@ -6,4 +6,9 @@ Tasks
 
 <?= $this->section('content') ?>
 <h1>Tasks</h1>
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+        <li><?= $task['id'] ?> <?= $task['description'] ?></li>
+    <?php endforeach; ?>
+</ul>
 <?= $this->endSection() ?>
